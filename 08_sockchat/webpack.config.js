103b2abdf6a7b011014a7e1162ac500569f9f9b7
Isel Tula <iseltula04@gmail.com>
2016-05-10 11:59:42 -0400

    socket.io Chat with ES6 and webpack

--- 08_sockchat/webpack.config.js
+++ 08_sockchat/webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry:[
-    './src/index.js'
-  ],
-  output:{
-    path: _dirname,
-    filename: 'app/js/main.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel',
-      exclude:/node_modules/
-    }]
-  }
+	entry: [
+		'./src/index.js'
+	],
+	output: {
+		path: __dirname,
+		filename: 'app/js/main.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			loader:'babel',
+			exclude: /node_modules/
+		}]
+	}
 }
